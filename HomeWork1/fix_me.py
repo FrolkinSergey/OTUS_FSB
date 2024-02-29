ab5a69e486a3d78e6ef7baee1d58d48d40b1269f
FrolkinSergeyOTUS <frolkin.sergey2012@gmail.com>
2024-02-29 15:53:02 +0300

    edit fix_me.py

--- HomeWork1/fix_me.py
+++ HomeWork1/fix_me.py
@@ -5,9 +5,10 @@ def calculate_average(nums):
     """Return average"""
     total = sum(nums)
     count = len(nums)
-    total / count
+
+    return total / count
 
 
 nums2 = [10, 15, 20]
-RESULT = calculate_average
+RESULT = calculate_average(nums2)
 print("The average is:", RESULT)
