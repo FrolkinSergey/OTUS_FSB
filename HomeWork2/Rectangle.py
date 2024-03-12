59d572beb3e77d250fca8a9b006fd4102351de03
FrolkinSergeyOTUS <frolkin.sergey2012@gmail.com>
2024-03-12 23:22:54 +0300

    pass HW2

---
+++ HomeWork2/Rectangle.py
@@ -0,0 +1,18 @@
+from abc import ABC
+from HomeWork2.Figure import Figure
+
+
+class Rectangle(Figure, ABC):
+
+    def __init__(self, side_a: int, side_b: int):
+        super().__init__(name="Rectangle")
+        if side_a <= 0 or side_b <= 0:
+            raise ValueError("Requires values greater than 0")
+        self.side_a = side_a
+        self.side_b = side_b
+
+    def get_area(self):
+        return self.side_a * self.side_b
+
+    def get_perimetr(self):
+        return 2 * (self.side_a + self.side_b)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OTUS_FSB
+HomeWork 1
+HomeWork 2
