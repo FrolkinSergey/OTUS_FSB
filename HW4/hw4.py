d213fe5da6a599e6492d4c823706366b67a4629f
FrolkinSergeyOTUS <frolkin.sergey2012@gmail.com>
2024-03-22 13:49:09 +0300

    pass HW4

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -1,41 +1,36 @@
+import itertools
 import json
-from HW4 import JSON_FILE_PATH, CSV_FILE_PATH
+from HW4 import JSON_FILE_PATH, CSV_FILE_PATH, JSON_RESULT_FILE_PATH
 from csv import DictReader
 
-with open(JSON_FILE_PATH, "r") as f:
-    users = json.load(f)
+
+with (open(CSV_FILE_PATH, "r") as books,
+      open(JSON_FILE_PATH, "r") as f,
+      open(JSON_RESULT_FILE_PATH, "w") as result_json):
+    books = DictReader(books)
+
+    users = [
+        {
+            "name": user["name"],
+            "gender": user["gender"],
+            "address": user["address"],
+            "age": user["age"],
+            "books": [],
+        }
+        for user in json.load(f)
+    ]
+
+    for book, user in zip(books, itertools.cycle(users)):
+        user["books"].append(
+            {
+                "title": book["Title"],
+                "author": book["Author"],
+                "pages": book["Pages"],
+                "genre": book["Genre"],
+            }
+        )
+        json.dump(users, result_json, indent=4)
 
 
-Lst = []
-Lst_books = []
-for user in users:
-    dict_temp = dict()
-    dict_temp['name'] = user['name']
-    dict_temp['gender'] = user['gender']
-    dict_temp['address'] = user['address']
-    dict_temp['age'] = user['age']
-    dict_temp['books'] = Lst_books
-    Lst.append(dict_temp)
-
-
-while Lst_books:
-    for item in Lst_books:
-        Lst.append(Lst_books[0])
-        Lst_books.pop(0)
-
-
-with open(CSV_FILE_PATH, newline='') as b:
-    reader = DictReader(b)
-    for book in reader:
-        bookdict = dict()
-        bookdict['title'] = book['Title']
-        bookdict['author'] = book['Author']
-        bookdict['pages'] = book['Pages']
-        bookdict['genre'] = book['Genre']
-        Lst_books.append(bookdict)
-
-
-with open("result.json", "w") as f:
-    json.dump(Lst, f, indent=4)
 
 
