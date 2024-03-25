de5e49295c606a4bf24bf1ac336122fc1c23a095
FrolkinSergeyOTUS <frolkin.sergey2012@gmail.com>
2024-03-25 16:55:24 +0300

    HW4

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -1,36 +1,43 @@
-import itertools
+import csv
 import json
-from HW4 import JSON_FILE_PATH, CSV_FILE_PATH, RESULT_JSON_FILE_PATH
-from csv import DictReader
+from HW4 import JSON_FILE_PATH, CSV_FILE_PATH, JSON_RESULT_FILE_PATH
 
 
-with (open(CSV_FILE_PATH, "r") as books,
-      open(JSON_FILE_PATH, "r") as f,
-      open(RESULT_JSON_FILE_PATH, "w") as result_json):
-    books = DictReader(books)
+dict_books = {}
+books_list = []
+users_list = []
+dict_users = {}
 
-    users = [
-        {
-            "name": user["name"],
-            "gender": user["gender"],
-            "address": user["address"],
-            "age": user["age"],
-            "books": [],
-        }
-        for user in json.load(f)
-    ]
+with open(JSON_FILE_PATH, 'r') as f:
+    users = json.load(f)
+    for user in users:
+        dict_users['name'] = user['name']
+        dict_users['gender'] = user['gender']
+        dict_users['address'] = user['address']
+        dict_users['age'] = user['age']
+        users_list.append(dict_users)
+        dict_users = {}
 
-    for book, user in zip(books, itertools.cycle(users)):
-        user["books"].append(
-            {
-                "title": book["Title"],
-                "author": book["Author"],
-                "pages": book["Pages"],
-                "genre": book["Genre"],
-            }
-        )
-        json.dump(users, result_json, indent=4)
+with open(CSV_FILE_PATH, newline='') as data:
+    for row in csv.DictReader(data):
+        dict_books['title'] = row.get('Title')
+        dict_books['author'] = row.get('Author')
+        dict_books['pages'] = int(row.get('Pages'))
+        dict_books['genre'] = row.get('Genre')
+        books_list.append(dict_books)
+        dict_books = {}
 
+while books_list:
+    for user in users_list:
+        if not books_list:
+            break
+        if 'books' in user:
+            users_books = user.get('books')
+        else:
+            user['books'] = []
+            users_books = user.get('books')
+        users_books.append(books_list.pop(0))
 
-
-
+with open(JSON_RESULT_FILE_PATH, "w") as final_json:
+    final_data = json.dumps(users_list, indent=4)
+    final_json.write(final_data)
